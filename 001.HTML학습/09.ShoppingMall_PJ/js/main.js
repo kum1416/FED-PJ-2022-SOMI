27f85aec734e2500655cf1ba762b31b09f4b6757
kum1416 <kum1416@naver.com>
2023-02-21 16:20:18 +0900

    Update main.js

--- 001.HTML학습/09.ShoppingMall_PJ/js/main.js
+++ 001.HTML학습/09.ShoppingMall_PJ/js/main.js
@@ -40,21 +40,47 @@ function loadFn() {
   console.log("로딩완료!");
 
 
-  // 슬라이드 개수 변수
-  let scnt = document.querySelectorAll("#slide>li").length;
-  console.log("슬개수:",scnt);
+  // 슬라이드 li리스트
+  let slist = document.querySelectorAll("#slide>li");
+
+  // 잘라내기로 li순번이 뒤섞이므로 블릿변경 매칭을 위한
+  // 고유순번을 사용자정의 속성(data-)으로 만들어준다!
+  slist.forEach((ele,idx)=>{
+    // data-seq 라는 사용자정의 속성 넣기
+    ele.setAttribute("data-seq",idx);
+  }); /////// forEach ///////////////////
 
   // 1. 대상선정
   // 1-1. 이벤트 대상: .abtn
   const abtn = document.querySelectorAll(".abtn");
-  // console.log(abtn);
-
+  
   // 1-2. 변경 대상: #slide
   const slide = document.querySelector("#slide");
+  
+  // 1-3. 블릿 대상: .indic li
+  const indic = document.querySelectorAll(".indic li");
+  console.log(indic);
+
+  // 광클금지변수 : 0 - 허용, 1 - 불허용
+  let prot = 0;
 
   // 2. 슬라이드 변경함수 만들기
+  // 호출시 seq에 들어오는 값중 1은 오른쪽, 0은 왼쪽
   const goSlide = (seq) => {
     console.log("슬고우!",seq);
+
+    console.log("못들어갔어!!!!");
+
+    // 광클금지 설정하기 ////
+    if(prot) return;
+    prot = 1; // 잠금!
+    setTimeout(() => {
+      prot = 0; // 해제!
+    }, 400); /// 0.4초후 해제! ///
+  
+
+
+    console.log("나,들어왔어!!!");
 
     // 0. 현자의 슬라이드 li수집하기
     let clist = slide.querySelectorAll("li");
@@ -98,10 +124,12 @@ function loadFn() {
 
       //(2) 동시에 left값을 -100%로 변경한다.
       slide.style.left = "-100%";
+      // 이때 트랜지션을 없앤다(한번실행후 부터 생기므로!)
+      slide.style.transition = "none";
       
       // (3) 그 후 left값을 0으로 애니메이션하여
       // 슬라이드가 왼쪽에서 들어온다.
-      // 동일 속성인 lefr가 같은 코딩처리 공간에 동시에
+      // 동일 속성인 left가 같은 코딩처리 공간에 동시에
       // 있으므로 이것을 분리해야 효과가 있다
       // setTimeout을 사용한다!
       setTimeout(() => {
@@ -111,6 +139,25 @@ function loadFn() {
       }, 0); ////// 타임아웃 //////////
 
     } ///////// else : 왼쪽클릭시 /////
+
+    // 2. 현재 슬라이드 순번과 같은 블릿표시하기
+    // 대상: .indic li -> indic변수
+    // 2-1. 현재 배너리스트 업데이트하기
+    clist = slide.querySelectorAll("li");
+    // 오른쪽클릭시(seq===1) 두번째슬라이드[1], 
+    // 왼쪽클릭시(seq===0) 첫번째 슬라이드[0]
+    // seq순번과 읽어올 슬라이드 순번이 일치한다!
+
+    // 2-2. 방향별 읽어올 슬라이드 순번으로 "data-seq"값 읽어오기
+    let cseq = clist[seq].getAttribute("data-seq")
+    console.log("현재순번:",cseq);
+
+    // 2-3. 블릿초기화
+    for(let x of indic) x.classList.remove("on");
+
+    // 2-4. 읽어온 슬라이드 순번의 블릿에 클래스 "on"넣기
+    indic[cseq].classList.add("on")
+
 
 
   }; /////////// goSlide함수 ////////////
