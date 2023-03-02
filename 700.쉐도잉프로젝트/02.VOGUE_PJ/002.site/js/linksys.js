7296a069b1c7fbe512ff66eefac9db51a8f9c1e1
kum1416 <kum1416@naver.com>
2023-03-02 12:54:14 +0900

    Update linksys.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -17,13 +17,13 @@ function linkFn(){
 
     // 2. 클릭 이벤트 설정하기
     // (1) GNB 클릭설정 /////////
-    for(let x of gnb){ //📢gnb만큼 돌아
+    for(let x of gnb){ //📢gnb만큼 돌아!
         x.onclick = (e) => { //📢무조건 하나의 이벤트를 보냄(?)
             // 클릭이동기능 막기
                 e.preventDefault(); //📢a태그 튀는 현상 막아줌
 
             // (1) 클릭된 a요소 텍스트 읽기
-            let atxt = x.innerText.toLowerCase().trim();
+            let atxt = x.innerText.toLowerCase().trim(); //📢mdata랑 맞추려고 소문자로 만들어줌
             // toLowerCase() -> 소문자변환 📢내장함수
             // 참고) toUpperCase() -> 대문자변환
             // trim() -> 앞뒤공백제거
@@ -31,8 +31,9 @@ function linkFn(){
 
             console.log(atxt);
 
-            // (2) 텍스트 변경하기
-
+            // (2) 서브 페이지 이동하기
+            location.href = "category.html?cat=" //📢받는데서 다시 변환한다(?)
+            +encodeURIComponent(atxt); //📢특수문자처리
 
         } /////// click //////
     } ///////// for of ////////////
