64c1c0cd89fb7a2fa97de5a0a61c6fc3d58ec29e
kum1416 <kum1416@naver.com>
2023-03-07 16:20:26 +0900

    js

--- 001.HTML학습/05.Nav_PJ/nav06/js/ban.js
+++ 001.HTML학습/05.Nav_PJ/nav06/js/ban.js
@@ -4,6 +4,28 @@ window.addEventListener("DOMContentLoaded",setBan);
 // 🥗배너셋팅함수 //////
 function setBan(){
     // 1. 호출확인
-    console.log("배너야~!");
+    // console.log("배너야~!");
+
+    // 2. 대상선정 : .bancont
+    const bancont = document.querySelector(".bancont");
+    // console.log(bancont);
+
+    // 3. 태그 구성하기
+    // 태그변수
+    let hcode = `<ul class="slide">`;
+    
+    for(let i = 1 ; i<= 13; i++){
+        hcode += ` 
+            <li>
+                <img src="./nav06/img_nav06/ban${i}.png" alt="배너이미지">
+            </li>
+        `;
+    } /////// for 문 ///////
+    
+    hcode += `</ul>`;
+    // console.log(hcode);
+
+    // 4. .bancont에 출력하기
+    bancont.innerHTML = hcode;
 
 } //////////// srtBan 함수 //////////
