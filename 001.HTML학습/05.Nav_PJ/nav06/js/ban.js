dc211ad982f88ac00128e21e0f9094ddf1f035f9
kum1416 <kum1416@naver.com>
2023-03-07 16:06:52 +0900

    Create ban.js

---
+++ 001.HTML학습/05.Nav_PJ/nav06/js/ban.js
@@ -0,0 +1,9 @@
+// 네비유형 6 : 배너셋팅 JS - ban.js //////
+window.addEventListener("DOMContentLoaded",setBan);
+
+// 🥗배너셋팅함수 //////
+function setBan(){
+    // 1. 호출확인
+    console.log("배너야~!");
+
+} //////////// srtBan 함수 //////////
