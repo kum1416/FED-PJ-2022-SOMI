a63ab3ae4339a7fa11cad097bb0df2c21d351504
kum1416 <kum1416@naver.com>
2023-03-20 10:20:15 +0900

    부드러운 스크롤

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/common.js
@@ -0,0 +1,10 @@
+// 보그 PJ - 공통 JS : common.js
+
+// 로드구역 ////////
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("공통JS 로딩완료!");
+
+    // 부드러운 스크롤 JS호출!
+    startSS();
+
+}); ////////////////// load /////////////////
