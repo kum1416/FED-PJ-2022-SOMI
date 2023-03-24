d6ee70d1385033ad67ccd705cf6297e33cc4d967
kum1416 <kum1416@naver.com>
2023-03-24 12:44:55 +0900

    ss

--- 004.JS학습/JS_Dallyeog/js/calendar.js
+++ 004.JS학습/JS_Dallyeog/js/calendar.js
@@ -1,5 +1,9 @@
 // 달력 생성자함수 /////
 
+
+// 호출
+MakeDallyeok();
+
 function MakeDallyeok(){
 
     // 선택함수 /////
@@ -20,6 +24,22 @@ function MakeDallyeok(){
     const monthTit = qs(".monthTit");
     // (5) 날짜요소 : .dates
     const dates = qs(".dates");
+
+    cg(dates);
+
+    // 2. 함수 만들기 //////////
+    // (1) 달력 초기화 구성 함수
+    const initDallyeok = () => {
+        // getMonth() 정보는 항상 현재달 숫자보다 1작음(배열순번임!)
+        // 1. 전달 마지막 날짜(옵션:0) -> 달력 전달끝쪽 날짜표시
+        const prevLast = new Date(curr_date.getFullYear(),curr_date.getMonth(),0);
+        cg(prevLast);
+
+    }; /////// initDallyeok 함수 /////
+
+    initDallyeok();
+
+
 
 
 } /////////////// MakeDallyeok ////////////////
