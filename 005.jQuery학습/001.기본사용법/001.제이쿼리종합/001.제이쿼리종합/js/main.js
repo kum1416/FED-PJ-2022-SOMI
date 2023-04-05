21ba59e3ea11eab4b3dbed026c60cc88c01a2a0f
kum1416 <kum1416@naver.com>
2023-04-05 12:46:13 +0900

    Update main.js

--- 005.jQuery학습/001.기본사용법/001.제이쿼리종합/001.제이쿼리종합/js/main.js
+++ 005.jQuery학습/001.기본사용법/001.제이쿼리종합/001.제이쿼리종합/js/main.js
@@ -1,11 +1,11 @@
 // 미니언즈 좀비 탈출 애니 구현 JS - main.js
 $(() => {
-  /////////// jQB ///////////////////
+    /////////// jQB ///////////////////
 
-  // 로딩확인
-  console.log("로딩완료!");
+    // 로딩확인
+    console.log("로딩완료!");
 
-  /*********************************** 
+    /*********************************** 
         [ 요구사항정리 ]
         1. 버튼을 클릭하여 미니언즈가
         순서대로 특정위치로 이동하며
@@ -26,92 +26,101 @@ $(() => {
 
     ***********************************/
 
-  // 0. 주인공들 변수에 할당!
-  // (1) 미니언즈
-  const mi = $(".mi");
+    // 0. 주인공들 변수에 할당!
+    // (1) 미니언즈
+    const mi = $(".mi");
 
-  // (2) 건물 li
-  const bd = $(".building li");
+    // (2) 건물 li
+    const bd = $(".building li");
 
-  // (3) 버튼들
-  const btns = $(".btns button");
+    // (3) 버튼들
+    const btns = $(".btns button");
 
-  // (4) 메시지박스
-  const msg = $(".msg");
+    // (4) 메시지박스
+    const msg = $(".msg");
 
-  // (5) 좀비, 주사기 요소 변수처리
-  let mz1 = `<img src="./images/mz1.png" alt="좀비1" class="mz">`;
-  let mz2 = `<img src="./images/mz2.png" alt="좀비2" class="mz">`;
-  let zom = `<img src="./images/zom.png" alt="좀비들" class="mz">`;
-  let inj = `<img src="./images/inj.png" alt="주사기" class="inj">`;
+    // (5) 좀비, 주사기 요소 변수처리
+    let mz1 = `<img src="./images/mz1.png" alt="좀비1" class="mz">`;
+    let mz2 = `<img src="./images/mz2.png" alt="좀비2" class="mz">`;
+    let zom = `<img src="./images/zom.png" alt="좀비들" class="mz">`;
+    let inj = `<img src="./images/inj.png" alt="주사기" class="inj">`;
 
-  // console.log(mi,bd,btns,msg);
+    // console.log(mi,bd,btns,msg);
 
-  // 1. 건물 각 방에 번호넣기 + 좀비/주사기 넣기
-  // 대상: .building li -> bd변수
-  // 사용제이쿼리 메서드:
-  // (1) each((순서,요소)=>{}) : 요소의 개수만큼 순서대로 돌아줌!
-  // (2) append(요소) : 요소내부에 자식요소 추가(이동)
+    // 1. 건물 각 방에 번호넣기 + 좀비/주사기 넣기
+    // 대상: .building li ->  bd변수
+    // 사용제이쿼리 메서드:
+    // (1) each((순서,요소)=>{}) : 요소의 개수만큼 순서대로 돌아줌!
+    // (2) append(요소) : 요소내부에 자식요소 추가(이동)
 
-  bd.each((idx, ele) => {
-    // console.log(idx,ele);
-    // 1. 각 방에 숫자로 순번넣기
-    $(ele).text(idx);
+    bd.each((idx, ele) => {
+        // console.log(idx,ele);
+        // 1. 각 방에 숫자로 순번넣기
+        $(ele).text(idx);
 
-    // 2. 좀비/주사기 넣기
-    switch (idx) {
-      case 9:
-        $(ele).append(mz1);
-        break;
-      case 7:
-        $(ele).append(mz2);
-        break;
-      case 1:
-        $(ele).append(zom);
-        break;
-      case 2:
-        $(ele).append(inj);
-        break;
-    } ///// switch case /////
-  }); ///// each //////
+        // 2. 좀비/주사기 넣기
+        switch (idx) {
+            case 9:
+                $(ele).append(mz1);
+                break;
+            case 7:
+                $(ele).append(mz2);
+                break;
+            case 1:
+                $(ele).append(zom);
+                break;
+            case 2:
+                $(ele).append(inj);
+                break;
+        } ////// switch case //////
+    }); /////////// each ///////////
 
-  // 좀비는 모두 숨기기
-  $(".mz").hide();
-  // 시간없는 hide()는 display:none처리함!
+    // 좀비는 모두 숨기기
+    $(".mz").hide();
+    // 시간없는 hide()는 display:none처리함!
 
-  // 2. 버튼셋팅하기 //////////////
-  // 대상: .btns buttons -> btns변수
-  btns.hide().first().show();
+    // 2. 버튼셋팅하기 ////////////////
+    // 대상: .btns buttons -> btns변수
+    btns.hide().first().show();
 
-  // 3. 공통함수 : actMini() ////////
-  const actMini = () => {};
+    // 3. 공통함수 : actMini() /////////
+    const actMini = () => {};
 
-  // 4. "들어아기" 버튼 클릭시
-  btns.first()
-  .click(function(){
-    // 1. 클릭된 버튼 사라지기
-    $(this).slideUp(300);
-    // 2. 메시지 없애기 : .msg -> msg변수
-    msg.fadeOut(300);
+    // 4. "들어가기" 버튼 클릭시
+    btns.first()
+    .click(function(){
+        // 1. 클릭된 버튼 사라지기
+        $(this).slideUp(300);
 
-    // 3. 이동하기
-    // 위치: li 8번방 -> bd변수에 있는 모든 li 중 8번
-    let room = bd.eq(8)
-    // 위치값 배열변수
-    let pos = [];
+        // 2. 메시지 없애기 : .msg -> msg변수
+        msg.fadeOut(300);
 
-    // top위치값
-    pos[0] = room.offset().top;
-    // left 위치값
-    pos[1] = room.offset().left;
+        // 3. 위치값 알아내기
+        // 위치: li 8번방 -> bd변수에 있는 모든 li 중 8번
+        let room = bd.eq(8);
+        // 위치값 배열변수
+        let pos = [];
 
-    // 제이쿼리 위치값 정보 메서드 : offset() -> 하위속성: top,left
+        // top위치값
+        pos[0] = room.offset().top;
+        // left위치값 : 방에서 중앙이동(+li가로크기절반-미니언즈가로크기절반)
+        pos[1] = room.offset().left + room.width()/2 - mi.width()/2;
+
+        // 제이쿼리 위치값 정보 메서드 : offset() -> 하위속성:top,left
+        // 제이쿼리 가로,세로 크기정보 메서드 : width(), height()
+
+        console.log(room,pos);
+
+        // 4. 미니언즈 이동하기
+        // 대상: .mi -> mi변수
+        mi.animate({
+            top: pos[0]+"px",
+            left:pos[1]+"px"
+        },800,"easeOutElastic")
 
 
-    console.log(room,pos);
 
-  })
-
+    })
 
 
 }); /////////////// jQB ////////////////////
