a160f72ff63010d240a5b5ffd8fd0fa8363a44e1
kum1416 <kum1416@naver.com>
2023-04-18 16:54:37 +0900

    Update main.js

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -6,7 +6,7 @@ import autoScroll from "./jquery-autoScroll.js";
 // 자동스크롤 호출
 autoScroll();
 
-// 메인 페이지
+// 메인 페이지 
 
 // 햄버거 버튼 클릭시 전체 메뉴 보이기
 $(".ham").click(function(){
@@ -14,4 +14,5 @@ $(".ham").click(function(){
     $(this).toggleClass("on");
     // 전체메뉴보이기
     $(".mbox").fadeToggle(400);
+
 })
