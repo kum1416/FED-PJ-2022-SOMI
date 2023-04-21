19ecc87085bfd99c0029dd7042a3f3a21996537f
kum1416 <kum1416@naver.com>
2023-04-21 10:05:08 +0900

    ss

---
+++ 004.JS학습/Parallax_PJ/js/main.js
@@ -0,0 +1,13 @@
+// 패럴렉스 PJ JS - main.js
+
+window.addEventListener("DOMContentLoaded",loadFn);
+
+// 로드함수 ////
+function loadFn(){
+
+    console.log("로딩완료!");
+
+    // 부드러운 스크롤 적용하기!
+    startSS();
+
+} //////////// loadFn //////////////////
