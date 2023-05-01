8dca01f1b299b00b46d74f352de15646979c421c
kum1416 <kum1416@naver.com>
2023-05-01 15:48:28 +0900

    Create 05.SFC.vue

---
+++ 010.VueJS학습/001.기초연습/05.SFC.vue
@@ -0,0 +1,23 @@
+<!-- html 템플릿코드 -->
+<template>
+    <div class="mytemp">
+        <span class="tit">{{ txt }}</span>
+    </div>
+</template>
+
+<!-- 자바스크립트 코드 -->
+<script>
+export default(await import("vue").undefinedComponent({
+    name:"myTemp",
+    data(){
+        return{
+            txt : "연습이에용~!"
+        }
+    }
+}))
+</script>
+
+<!-- 스타일시트 코드 -->
+<style scoped> /* scoped 여기에만 적용된다 */
+    span.tit {color:red}
+</style>
