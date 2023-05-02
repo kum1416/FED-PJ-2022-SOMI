7cd403fbe78739cab57985ab5b2740582e492cd8
kum1416 <kum1416@naver.com>
2023-05-02 10:21:05 +0900

    SS

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -9,7 +9,7 @@ import comData from "./tempData/data-common.js";
 //###### 상단영역 메뉴 뷰 템플릿 셋팅하기 #######
 // Vue.component(내가지은요소명,{옵션})
 Vue.component("top-comp",{
-    template:comData.tarea,
+    template:comData.tareaSub,
 }); ////////// 상단영역 Vue component //////////
 
 //###### 하단영역 메뉴 뷰 템플릿 셋팅하기 #######
