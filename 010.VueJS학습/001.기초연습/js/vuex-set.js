ab2157e055b997d07f8778bd3e0bcb702a9fbe22
kum1416 <kum1416@naver.com>
2023-05-03 16:20:53 +0900

    Update vuex-set.js

--- 010.VueJS학습/001.기초연습/js/vuex-set.js
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -1,2 +1,32 @@
 // 뷰엑스 스토어 구현 JS 
 
+// 1. 상단영역 컴포넌트 셋팅
+Vue.component("top-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    mathods:{
+
+    }
+});
+// 2. 메인영역 컴포넌트 셋팅
+Vue.component("main-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    mathods:{
+
+    }
+});
+// 3. 하단영역 컴포넌트 셋팅
+Vue.component("info-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    mathods:{
+
+    }
+});
