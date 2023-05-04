e610d8b0ccc672f3e39873a04b655f411955024c
kum1416 <kum1416@naver.com>
2023-05-04 13:14:39 +0900

    Update vuex-set.js

--- 010.VueJS학습/001.기초연습/js/vuex-set.js
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -124,11 +124,11 @@ new Vue({
 
             // 이미지와 설명박스 순서대로 나타나기
             $("main img")
-            .css({opacity:0})
+            .css({opacity:0}).stop()
             .delay(500).fadeTo(500,1);
 
             $("main p")
-            .css({opacity:0})
+            .css({opacity:0}).stop()
             .delay(1000).fadeTo(500,1);
 
         } //// showBx ////
