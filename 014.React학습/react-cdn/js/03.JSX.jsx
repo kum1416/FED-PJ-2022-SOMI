f7c27a3fa9b0e24c91b5363ae389805af228893b
kum1416 <kum1416@naver.com>
2023-05-23 12:49:33 +0900

    Update 03.JSX.jsx

--- 014.React학습/react-cdn/js/03.JSX.jsx
+++ 014.React학습/react-cdn/js/03.JSX.jsx
@@ -109,7 +109,7 @@ const mydata = [
 // const mylist = mydata.map((val,idx,obj)=>
 const mylist = mydata.map(val=>
     <li>
-        {val.name} : {val.movie} ★ {val.dix==3?"예뻐":"멋쪄!"}
+       {val.name} : {val.movie} ★ {val.idx==3?"예뻐":"멋쪄!"}
     </li>
 );
 
@@ -139,12 +139,11 @@ ReactDOM.render(myele6,
 
 /***************************************************** 
     [ JSX에서 속성 클래스는 className 으로 표기한다! ]
-    <태그 class="클래스명"
+    <태그 class="클래스명">
     class는 JS에서 키워드이므로 사용못함! 대신...
     <태그 className="클래스명">
 *****************************************************/
-const myele7 = <h1 className="myclass">className 속성으로 클래스셋팅!</h1>
-
+const myele7 = <h1 className="myclass">className 속성으로 클래스 셋팅!</h1>;
 
 // 일곱번째 div요소에 출력하기
 ReactDOM.render(myele7, 
@@ -153,10 +152,10 @@ ReactDOM.render(myele7,
 /***************************************************** 
     [ JSX에서 조건문 사용하기 - if문 ]
     리액트는 if명령문을 지원하지만
-    JSX내부에서는 지원하지 않는다!!!!
+    JSX내부에서는 지원하지 않는다!!!
 
     JSX에서 조건문을 사용하려면?
-    JSX 외부에서 if문을 사용하거나 
+    JSX 외부에서 if문을 사용하거나
     아니면 내부에서 삼항연산자를 사용할 수 있다!
 *****************************************************/
 // JSX 외부에서 if문 사용하기
@@ -164,7 +163,7 @@ const x = 100000;
 let txt = "이 돈으로는 충분히 살 수 있어!";
 if(x<10000){
     txt = "돈이 부족해서 살 수 없어!"
-} //// if ////
+} //// if ///////
 
 const myele8 = <div>
     <h1>현재 내가 가진 돈은 {x}원!</h1>
@@ -177,9 +176,6 @@ const myele8 = <div>
 ReactDOM.render(myele8, 
     document.querySelectorAll("#root>div")[7]);
 
-/***************************************************** 
-
-*****************************************************/
 
 /***************************************************** 
 
