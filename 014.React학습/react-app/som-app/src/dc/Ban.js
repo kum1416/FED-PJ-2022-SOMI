b086c6b557a75f73667d7aa53ed1e6ebb96a612e
kum1416 <kum1416@naver.com>
2023-06-01 15:04:48 +0900

    ss

--- 014.React학습/react-app/som-app/src/dc/Ban.js
+++ 014.React학습/react-app/som-app/src/dc/Ban.js
@@ -1,19 +1,31 @@
-
+// 배너 컴포넌트 - Ban.js
+import "./css/ban.css";
 
 function Ban() {
-    return (
-        <>
-            <img src="./images/dcm21.jpg" alt="배너" />
-            <section className="bantit">
-                <h3>GOTHAM GAZETTE</h3>
-                <h2>WORLDS TRAVELER</h2>
-                <p>
-                    Barry who? The Flash isn't the only DC hero this summer who's been traveling
-                    through the multiverse.
+
+    const ban_data = [
+        {},
+        {}
+    ];
+
+
+  return (
+    <div className="banner">
+      <ul className="slider">
+        <li>
+          <img className="banimg" src="./images/dcm21.jpg" alt="배너" />
+          <section className="bantit">
+            <h3>GOTHAM GAZETTE</h3>
+            <h2>WORLDS TRAVELER</h2>
+            <p>
+                Barry who? The Flash isn't the only DC hero this summer who's been traveling through the multiverse.
                 </p>
-            </section>
-        </>
-    );
+                <button>New Places, Familiar Faces</button>
+          </section>
+        </li>
+      </ul>
+    </div>
+  );
 } /////////// Ban 컴포넌트 /////////////
 
 export default Ban;
