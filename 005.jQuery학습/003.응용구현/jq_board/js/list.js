60224f659fa5cac0be242ec626599db4ce4c71f1
kum1416 <kum1416@naver.com>
2023-06-01 11:58:36 +0900

    ss

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -54,13 +54,45 @@ function bindList(pgnum){ // pgnum - 페이지번호
     $("#board tbody").html(blist);
 
     // 3. 페이징 블록 만들기
-    // 전체 페이지 번호수 계산하기
+    // 3-1.전체 페이지 번호수 계산하기
     // 전체레코드수 / 페이지단위수 (나머지있으면+ 1 )
     // 전체 레코드 수 : bdata.length
     let pgtotal = Math.floor(bdata.length / pgblock);
     let pgadd = bdata.length % pgblock;
     console.log("페이징 전체수:",pgtotal);
     console.log("페이징 나머지:",pgadd);
+
+    // 페이징코드변수
+    let pgcode = "";
+    
+    // 3-2. 페이징코드 만들기
+    // 나머지가 있으면 1을 더함
+    if(pgadd!=0) pgtotal = pgtotal+1;
+
+    // 코드만들기 for문
+    for(let i = 1; i <= pgtotal; i++){
+
+        pgcode += 
+        // 페이지번호와 i가 같으면 a링크를 만들지 않는다!
+        pgnum == i ? i : `<a href="#">${i}</a>`;
+
+        // 사이구분자(마지막번호 뒤는 제외)
+        if(i!=pgtotal) pgcode += " | ";
+       
+
+    } /////////// for문 //////////////
+
+    // 3-3. 페이징코드 넣기
+    $(".paging").html(pgcode);
+
+    // 3-5. 이벤트링크 생성하기
+    $(".paging a").click(function(e){
+        // 기본이동막기
+        e.preventDefault();
+        // 바인딩함수 호출!(페이지번호 보냄)
+        bindList($(this).text());
+        
+    }); /////////// click ///////////
 
 
 } /////////////// bindList함수 ///////////////
