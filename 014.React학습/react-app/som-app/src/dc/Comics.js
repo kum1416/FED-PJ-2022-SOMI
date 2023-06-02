27a7646f89186dab389b9615ba58773a28c91065
kum1416 <kum1416@naver.com>
2023-06-02 15:16:07 +0900

    ss

---
+++ 014.React학습/react-app/som-app/src/dc/Comics.js
@@ -0,0 +1,13 @@
+// DC Comics 페이지 컴포넌트
+import React from "react"; // 리액트 문법을 쓰고있으니까😀
+import Ban from "./Ban";
+
+const Comics = () => {
+    return(
+        <>
+            <Ban cat="COMICS" />
+        </>
+    );
+}; ////////// Comics ///////////
+
+export default Comics;
