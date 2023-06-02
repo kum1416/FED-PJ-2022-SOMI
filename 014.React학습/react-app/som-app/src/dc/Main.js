3f00c466ff0727c36fe59a03fa0c9109068c6f10
kum1416 <kum1416@naver.com>
2023-06-02 15:08:17 +0900

    ss

---
+++ 014.React학습/react-app/som-app/src/dc/Main.js
@@ -0,0 +1,13 @@
+// DC 메인 페이지 컴포넌트
+import React from "react"; // 리액트 문법을 쓰고있으니까😀
+import Ban from "./Ban";
+
+const Main = () => {
+    return(
+        <>
+            <Ban cat="main" />
+        </>
+    );
+}; ////////// Main ///////////
+
+export default Main;
