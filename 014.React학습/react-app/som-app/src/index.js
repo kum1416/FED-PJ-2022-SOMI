d1851a94462f7964e513f793082e8321d1e6d1c8
kum1416 <kum1416@naver.com>
2023-06-02 14:46:47 +0900

    ss

--- 014.React학습/react-app/som-app/src/index.js
+++ 014.React학습/react-app/som-app/src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "./dc/Layout";
+import "./index.css";
 
 /********************************************* 
     [ 리액트 라우터 ]
