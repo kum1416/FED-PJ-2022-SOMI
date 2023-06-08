b1646014b969893b29b86b23de35acf1d254606c
kum1416 <kum1416@naver.com>
2023-06-08 13:17:00 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/swipervid.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/swipervid.js
@@ -3,79 +3,78 @@
 
 const swipervid_data = [
     {
-        isrc:"./images/vidswp/vidsw1.jpg",
+        isrc:"./images/vidswp/vidswp1.jpg",
         vsrc:"https://www.youtube.com/embed/nI9uUv6AdoY",
         cat:"OFFICIAL TRAILER",
         tit:"My Adventures With Superman",
     },
     {
-        isrc:"./images/vidswp/vidsw2.jpg",
+        isrc:"./images/vidswp/vidswp2.jpg",
         vsrc:"https://www.youtube.com/embed/JWYKJGMlb4M",
         cat:"Behind the Scenes",
         tit:`Titans - "Heroes in Love"`,
     },
     {
-        isrc:"./images/vidswp/vidsw3.jpg",
+        isrc:"./images/vidswp/vidswp3.jpg",
         vsrc:"https://www.youtube.com/embed/WWv23TEvjfU",
         cat:"Behind the Scenes",
         tit:`Titans - "Baptism of Blood"`,
     },
     {
-        isrc:"./images/vidswp/vidsw4.jpg",
+        isrc:"./images/vidswp/vidswp4.jpg",
         vsrc:"https://www.youtube.com/embed/QX4X1UBd8U4",
         cat:"Exclusive Clip",
         tit:`Gotham Knights - "Art Burglary"`,
     },
     {
-        isrc:"./images/vidswp/vidsw5.jpg",
-        vsrc:"",
+        isrc:"./images/vidswp/vidswp5.jpg",
+        vsrc:"https://www.youtube.com/embed/fVWdbPczJuQ",
         cat:"Exclusive Clip",
         tit:`Justice League x RWBY: Super Heroes & Huntsmen, Part One - "Leadership"`,
     },
     {
-        isrc:"./images/vidswp/vidsw6.jpg",
+        isrc:"./images/vidswp/vidswp6.jpg",
         vsrc:"https://www.youtube.com/embed/xXyuUbtpmxE",
         cat:"Movies & TV",
-        tit:"Superman (1978) - Modern Trailer Recut",
+        tit:`Superman (1978) - Modern Trailer Recut`,
     },
     {
-        isrc:"./images/vidswp/vidsw7.jpg",
+        isrc:"./images/vidswp/vidswp7.jpg",
         vsrc:"https://www.youtube.com/embed/vS3_72Gb-bI",
         cat:"MOVIES & TV",
-        tit:"BLUE BEETLE - OFFICIAL TRAILER",
+        tit:`BLUE BEETLE - OFFICIAL TRAILER`,
     },
     {
-        isrc:"./images/vidswp/vidsw8.jpg",
+        isrc:"./images/vidswp/vidswp8.jpg",
         vsrc:"https://www.youtube.com/embed/Yw91jQwJnDo",
         cat:"MOVIES & TV",
-        tit:"MY ADVENTURES WITH SUPERMAN - OFFICIAL TEASER",
+        tit:`MY ADVENTURES WITH SUPERMAN - OFFICIAL TEASER`,
     },
     {
-        isrc:"./images/vidswp/vidsw9.jpg",
+        isrc:"./images/vidswp/vidswp9.jpg",
         vsrc:"https://www.youtube.com/embed/PQHoxEE-AqQ",
         cat:"Movies & TV",
-        tit:"BATMAN: THE DOOM THAT CAME TO GOTHAM - EXCLUSIVE CLIP",
+        tit:`BATMAN: THE DOOM THAT CAME TO GOTHAM - EXCLUSIVE CLIP`,
     },
     {
-        isrc:"./images/vidswp/vidsw10.jpg",
+        isrc:"./images/vidswp/vidswp10.jpg",
         vsrc:"https://www.youtube.com/embed/uGGjQH4YLPU",
         cat:"Comics",
-        tit:"Welcome to the Dawn of DC - Comic Trailer",
+        tit:`Welcome to the Dawn of DC - Comic Trailer`,
     },
     {
-        isrc:"./images/vidswp/vidsw11.jpg",
-        vsrc:"https://www.youtube.com/embed/vPib9KwCBGs",
+        isrc:"./images/vidswp/vidswp11.jpg",
+        vsrc:"https://www.youtube.com/embed/Qyv0jxOyUl0",
         cat:"Movies & TV",
-        tit:"Titans: The Final Episodes - Official Trailer",
+        tit:`Titans: The Final Episodes - Official Trailer`,
     },
     {
-        isrc:"./images/vidswp/vidsw12.jpg",
-        vsrc:"https://www.youtube.com/embed/AIc671o9yCI",
+        isrc:"./images/vidswp/vidswp12.jpg",
+        vsrc:"https://www.youtube.com/embed/Zi88i4CpHe4",
         cat:"EXCLUSIVE INTERVIEW",
-        tit:"SHAZAM! FURY OF THE GODS - ZACH LEVI",
+        tit:`SHAZAM! FURY OF THE GODS - ZACH LEVI`,
     },
 ];
-
 
 
 
