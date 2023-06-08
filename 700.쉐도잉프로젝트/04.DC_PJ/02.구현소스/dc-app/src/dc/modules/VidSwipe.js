ca41a5ae1cc22fd342cc95b50f3f6ed5cfe99c76
kum1416 <kum1416@naver.com>
2023-06-08 11:23:19 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidSwipe.js
@@ -0,0 +1,32 @@
+/// VidSwipe 모듈 - VidSwipe.js
+import $ from 'jquery';
+import "../css/VidSwipe.css";
+import VidSwipe from "./modules/VidSwipe";
+
+// 제이쿼리 로드구역 함수 /////////
+function jqFn(){
+    $(()=>{
+
+    }); /////// jQB ////////
+} ///////////// jqFn /////////////
+
+function MenuBtn(props){
+    // props.pg - 페이지별 데이터구분
+    return(
+        <>
+        {/* 모듈코드 */}
+        <SwiperVid name="나는" />
+
+        {/* 2. 비디오 재생창 */}
+        <section className='playvid'>
+            <iframe src=''></iframe>
+            <button className='cbtn'>×</button>
+        </section>
+
+        {/* 빈루트를 만들고 JS로드함수포함 */}
+        {jqFn()}
+        </>
+    )
+}
+
+export default Temp;
