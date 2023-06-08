01205813104ba5dd238a676a9abe30b94aac3647
kum1416 <kum1416@naver.com>
2023-06-08 11:44:55 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidSwipe.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidSwipe.js
@@ -12,19 +12,23 @@ function jqFn(){
 
 function VidSwipe(props){
     // props.pg - 페이지별 데이터구분
+    // props.tit - 모듈타이틀
     return(
         <>
         {/* 모듈코드 */}
+        <section className='vidswbox'>
+            {/* 1. 모듈타이틀 */}
+            <h2 className='tit'>{props.tit}</h2>
 
-        {/* 1. 스와이퍼 컴포넌트 */}
-        <SwiperVid name="나는" />
+            {/* 2. 스와이퍼 컴포넌트 */}
+            <SwiperVid name="나는" />
 
-        {/* 2. 비디오 재생창 */}
-        <section className='playvid'>
-            <iframe src=''></iframe>
-            <button className='cbtn'>×</button>
+            {/* 3. 비디오 재생창 */}
+            <section className='playvid'>
+                <iframe src=''></iframe>
+                <button className='cbtn'>×</button>
+            </section>
         </section>
-
         {/* 빈루트를 만들고 JS로드함수포함 */}
         {jqFn()}
         </>
