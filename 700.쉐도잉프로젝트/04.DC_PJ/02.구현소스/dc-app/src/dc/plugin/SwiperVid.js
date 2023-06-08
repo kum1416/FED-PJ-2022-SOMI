0488f29032af69718cf137acb02bb2b31c8ae947
kum1416 <kum1416@naver.com>
2023-06-08 10:56:44 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperVid.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./swipervid.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function SwiperVid(props) {
+
+    const data = ["허허","호호","크크","포포","파파"];
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={20}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+            data.map((v,i)=>
+            <SwiperSlide key={i}>{props.name} {v}</SwiperSlide>
+            )
+        }
+      </Swiper>
+    </>
+  );
+}
