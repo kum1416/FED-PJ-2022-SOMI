45fea57c57ddc780ca339b9869f2ad96fa2410a0
kum1416 <kum1416@naver.com>
2023-06-08 12:00:06 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperVid.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperVid.js
@@ -10,25 +10,26 @@ import "./swipervid.css";
 
 // import required modules
 import { Navigation } from "swiper";
+// 데이터 가져오기
+import swipervid_data from "../data/swipervid";
 
 export default function SwiperVid(props) {
-
-    const data = ["허허","호호","크크","포포","파파"];
+  // 데이터 셋팅
+  const sdt = swipervid_data;
 
   return (
     <>
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={20}
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-      >
-        {
-            data.map((v,i)=>            
-            <SwiperSlide key={i}>{props.name} {v}</SwiperSlide>
-            )
-        }
+      <Swiper 
+      slidesPerView={4} 
+      spaceBetween={20} 
+      navigation={true} 
+      modules={[Navigation]} 
+      className="mySwiper">
+        {sdt.map((v, i) => (
+          <SwiperSlide key={i}>
+            {props.name} {v.tit}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
