3c68ae56a706d1241013711d3db91ad74c196790
kum1416 <kum1416@naver.com>
2023-06-09 10:14:48 +0900

    Update Layout.js

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
@@ -12,7 +12,44 @@ import { Link, Outlet } from "react-router-dom";
     -> 라우터 연결 컴포넌트 출력자리 컴포넌트
 *******************************************************/
 
+/* sub:[{
+    txt:"",
+    link:""
+}] */
 const Layout = () => {
+
+    /* GNB메뉴 데이터구성하기 */
+    const gnb_data = [
+        {
+            txt:"Home",
+            link:"/",
+        },
+        {
+            txt:"CHARACTERS",
+            link:"/ct",
+        },
+        {
+            txt:"COMICS",
+            link:"/co",
+        },
+        {
+            txt:"MOVIES & TV",
+            link:"/mv",
+        },
+        {
+            txt:"GAMES",
+            link:"/gm",
+        },
+        {
+            txt:"NEWS",
+            link:"/nw",
+        },
+        {
+            txt:"VIDEO",
+            link:"/vd",
+        },
+    ];
+
     return (
         <>
             {/* 1.상단영역 */}
@@ -23,28 +60,14 @@ const Layout = () => {
                         <li>
                             <Logo />
                         </li>
+                        {
+                            gnb_data.map((v,i)=>
+                                <li key={i}>
+                                    <Link to={v.link}>{v.txt}</Link>
+                                </li>
+                            )
+                        }
 
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/ct">CHARACTERS</Link>
-                        </li>
-                        <li>
-                            <Link to="/co">COMICS</Link>
-                        </li>
-                        <li>
-                            <Link to="/mv">MOVIES & TV</Link>
-                        </li>
-                        <li>
-                            <Link to="/gm">GAMES</Link>
-                        </li>
-                        <li>
-                            <Link to="/nw">NEWS</Link>
-                        </li>
-                        <li>
-                            <Link to="/vd">VIDEO</Link>
-                        </li>
                     </ul>
                 </nav>
             </header>
