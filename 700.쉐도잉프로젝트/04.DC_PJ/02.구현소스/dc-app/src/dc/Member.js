d5a154bbafc55f33677df391418502e01472c61b
kum1416 <kum1416@naver.com>
2023-06-12 13:02:19 +0900

    Update Member.js

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Member.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Member.js
@@ -6,8 +6,8 @@ import "./css/member.css";
 /* 
     [ 후크 : Hook - 왜 필요한가? ]
     1. 목적 - 어떤 특정 데이터가 변경될때
-    다른 것을 매칭하여 실시간으로 변경할 필요가 있을 경우
-    간단하고 효과적으로 이것을 구현해주는 방법이다!
+        다른 것을 매칭하여 실시간으로 변경할 필요가 있을 경우
+        간단하고 효과적으로 이것을 구현해주는 방법이다!
     2. 구현방법
         1) 상단에 후크를 import 한다 -> useState
         2) useState() 메서드를 사용한다
@@ -29,13 +29,13 @@ import "./css/member.css";
 
 // 제이쿼리 로드구역 함수 /////////
 function jqFn() {
-  $(() => {}); //////// jQB ///////////
+    $(() => {}); //////// jQB ///////////
 } ////////////// jQFn ///////////
 
 function Member() {
 
     // 요구사항 : 각 입력항목에 맞는 유효성검사를 입력하는 순간!
-    //              실시간으로 체크하여 결과를 화면에 리턴한다!
+    //            실시간으로 체크하여 결과를 화면에 리턴한다!
 
     // [ 후크 useState 메서드 셋팅하기 ]
     // [ 1. 입력요소 후크변수 ]
@@ -63,13 +63,14 @@ function Member() {
     // 5. 이메일에러변수
     const [emailError,setEmailError] = useState(false);
 
+
     // [ 3. 유효성 검사 메서드 ]
     // 1. 아이디 유효성 검사
     const changeUserId = e => { // e - 이벤트전달변수
-        // 아이디 유효성 검사식(따옴표싸지 말것!)
+        // 1. 아이디 유효성 검사식(따옴표싸지 말것!)
         const valid = /^[A-Za-z0-9+]{5,}$/;
 
-        // 입력값 확인 : e.target -> 이벤트가 발생한 요소
+        // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
         console.log(e.target.value);
 
         // 3. 에러아님 상태 if문
@@ -81,19 +82,17 @@ function Member() {
         else
             setUserIdError(true); // 에러상태임!
 
-        // 4. 실제 userId 후크변수값이 업데이트 되어야 화면에 출력됨!
+        // 4. 실제 useerId 후크변수값이 업데이트 되어야 화면에 출력됨!
         setUserId(e.target.value);
 
-
-    }; //////////////// changeUserId ////////////////
+    }; /////////////// changeUserId ////////////////
 
     // 2. 비밀번호 유효성 검사
-    const changePwd = e => {
-
+    const changePwd = e => { // e - 이벤트전달변수
         // 1. 유효성 검사식(따옴표싸지 말것!)
         const valid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-        // 입력값 확인 : e.target -> 이벤트가 발생한 요소
+        // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
         console.log(e.target.value);
 
         // 3. 에러아님 상태 if문
@@ -105,67 +104,70 @@ function Member() {
         else
             setPwdError(true); // 에러상태임!
 
-        // 4. 실제 pwd 후크변수값이 업데이트 되어야 화면에 출력됨!
+        // 4. 실제 useerId 후크변수값이 업데이트 되어야 화면에 출력됨!
         setPwd(e.target.value);
-        
-    }; //////////// changePwd /////////////
-  
+
+
+    }; ///////////// changePwd ///////////////////
 
 
 
 
-  return (
-    <>
-      {/* 모듈코드 */}
-      <section className="membx">
-        <h2>Member</h2>
-        <form>
-          {/* 1.아이디 */}
-          <label>아이디를 : </label>
-          <input type="text" maxLength="20" placeholder="아이디를 입력하세요" value={userId} onChange={changeUserId} />
-          {
-            // 에러일 경우 메시지 보여주기
-            // 조건문 && 요소 -> 조건이 true이면 요소출력
-            userIdError &&
-            <div className="msg">
-              <small style={{ color: "red", fontSize: "10px" }}>
-                사용자 아이디는 5글자 이상 문자 또는 숫자를 포함해야 합니다!
-              </small>
-            </div>
 
-            // value={userId} 값은 setUserId를 통해서만
-            // 업데이트되어 실제화면에 반영된다!
 
-            // onChange={changeUserId}
-            // -> change이벤트 발생시 changeUserId 함수호출!
-          }
+    return (
+        <>
+            {/* 모듈코드 */}
+            <section className="membx">
+                <h2>Member</h2>
+                <form>
+                    {/* 1.아이디 */}
+                    <label>아이디 : </label>
+                    <input type="text" maxLength="20" placeholder="아이디를 입력하세요" value={userId} onChange={changeUserId} />
+                    {
+                        // 에러일 경우 메시지 보여주기
+                        // 조건문 && 요소 -> 조건이 true이면 요소출력
+                        userIdError &&
+                        <div className="msg">
+                            <small style={{ color: "red", fontSize: "10px" }}>
+                                사용자 아이디는 5글자 이상 문자 또는 숫자를 포함해야 합니다!
+                            </small>
+                        </div>
 
-          {/* 2.비밀번호 */}
-          <label>비밀번호 : </label>
-          <input type="password" maxLength="20" placeholder="비밀번호를 입력하세요" value={pwd} onChange={changePwd} />
-          {
-            // 에러일 경우 메시지 보여주기
-            // 조건문 && 요소 -> 조건이 true이면 요소출력
-            pwdError &&
-            <div className="msg">
-              <small style={{ color: "red", fontSize: "10px" }}>
-              비밀번호는 8자 이상이어야 하며 문자와 숫자를 
-              각각 하나 이상 포함해야 합니다
-              </small>
-            </div>
-          }
+                        // value={userId} 값은 setUserId를 통해서만
+                        // 업데이트되어 실제화면에 반영된다!
 
-          {/* 3.이름 */}
-          {/* 4.이메일 */}
-          {/* 5.버튼 */}
-          {/* 6.로그인페이지링크 */}
-        </form>
-      </section>
+                        // onChange={changeUserId} 
+                        // -> change이벤트 발생시 changeUserId 함수호출!
+                    }
 
-      {/* 빈루트를 만들고 JS로드함수포함 */}
-      {jqFn()}
-    </>
-  );
+                    {/* 2.비밀번호 */}
+                    <label>비밀번호 : </label>
+                    <input type="password" maxLength="20" placeholder="비밀번호를 입력하세요" value={pwd} onChange={changePwd} />
+                    {
+                        // 에러일 경우 메시지 보여주기
+                        // 조건문 && 요소 -> 조건이 true이면 요소출력
+                        pwdError &&                        
+                        <div className="msg">
+                            <small style={{ color: "red", fontSize: "10px" }}>
+                            비밀번호는 8자 이상이어야 하며 문자와 숫자를 
+                            각각 하나 이상 포함해야 합니다.
+                            </small>
+                        </div>
+                    }
+
+                    
+                    {/* 3.이름 */}
+                    {/* 4.이메일 */}
+                    {/* 5.버튼 */}
+                    {/* 6.로그인페이지링크 */}
+                </form>
+            </section>
+
+            {/* 빈루트를 만들고 JS로드함수포함 */}
+            {jqFn()}
+        </>
+    );
 }
 
 export default Member;
