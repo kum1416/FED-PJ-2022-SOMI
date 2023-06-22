0f5d9b3a7d1cf3f423a3e6e065ce65f29b62bbc0
kum1416 <kum1416@naver.com>
2023-06-22 12:53:29 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/CatList.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/CatList.js
@@ -10,11 +10,25 @@ function jqFn(){
     }); /////// jQB ////////
 } ///////////// jqFn /////////////
 
-function CatList(){
+function CatList(props){
+
+    // 선택데이터
+    let sdt = props.dt;
+
     return(
         <>
         {/* 모듈코드 */}
-        
+        <ul className='clist'>
+        {
+            sdt.map((v,i)=>
+                <li key={i}>
+                    <img src={v.tmsrc} alt={v.cname} />
+                    <h3>{v.cname}</h3>
+                </li>
+            )
+        }
+        </ul>
+        {/* {console.log(sdt)} */}
         {/* 빈루트를 만들고 JS로드함수포함 */}
         {jqFn()}
         </>
