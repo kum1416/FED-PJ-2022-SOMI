64b6f27d371c67cb967348e43456b09c658fac63
kum1416 <kum1416@naver.com>
2023-06-27 10:07:09 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
@@ -8,7 +8,6 @@ import $ from "jquery";
 import "swiper/css";
 import "swiper/css/navigation";
 
-
 import "./swipercat.css";
 
 // import required modules
@@ -18,68 +17,72 @@ import cat_data from "../data/cat";
 import { Link } from "react-router-dom";
 
 export default function SwiperCat(props) {
-    // 데이터 셋팅
-    const sdt = cat_data;  
-    console.log(sdt);  
+  // 데이터 셋팅
+  const sdt = cat_data;
+  console.log(sdt);
 
-    return (
-        <>
-            <Swiper
-                // slidesPerView={2}
-                spaceBetween={20}
-                navigation={true}
-                modules={[Navigation]}
-                // 스와이퍼 사이즈별 슬라이드수 변경!
-                breakpoints={{
-                    200: {
-                        slidesPerView: 3,
-                    },
-                    700: {
-                        slidesPerView: 4,
-                    },
-                    1000: {
-                        slidesPerView: 5,
-                    },
-                    1200: {
-                        slidesPerView: 7,
-                    },
-                }}
-                className="mySwiper">
-                {sdt.map((v, i) => (
-                    <SwiperSlide key={i}>
-                        {/* "/det" 라우터 컴포넌트 페이지 호출시
-                        state속성값으로 객체를 보내어 값을 전달함!
-                        도착페이지인 Detail.js 컴포넌트에
-                        페이지 나타내야할 데이터 항목을
-                        데이터 속성명과 같은 이름으로 셋팅하여
-                        라우터 전달 state객체에 담아서 보낸다!
-                        cname은 캐릭터 이름
-                        cdes 캐릭터설명
-                        cdes 캐릭터설명
-                        facts는 캐릭터명세 정보임!*/}
-                        <Link to="/det" 
-                        state={
-                            {
-                            cname:v.cname,
-                            cdesc:v.cdesc,
-                            facts:v.facts
-                            }
-                        }>
-                            <section className="swinbx">
-                                {/* 캐릭터이미지영역 */}
-                                <div className="catimg">
-                                    <img src={v.tmsrc} alt={v.cname} />
-                                    
-                                </div>
-                                {/* 동영상타이틀영역 */}
-                                <div className="cattit">
-                                    <h3>{v.cname}</h3>
-                                </div>
-                            </section>
-                        </Link>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        // slidesPerView={2}
+        spaceBetween={20}
+        navigation={true}
+        modules={[Navigation]}
+        // 스와이퍼 사이즈별 슬라이드수 변경!
+        breakpoints={{
+          200: {
+            slidesPerView: 3,
+          },
+          700: {
+            slidesPerView: 4,
+          },
+          1000: {
+            slidesPerView: 5,
+          },
+          1200: {
+            slidesPerView: 7,
+          },
+        }}
+        className="mySwiper"
+      >
+        {sdt.map(
+          (v, i) =>
+            /* 저스티스리그 캐릭터는 idx 1~7 */
+            Number(v.idx) <= 7 && ( //😀7보다 작거나 같으면 스와이퍼 출력
+              <SwiperSlide key={i}>
+                {/* "/det" 라우터 컴포넌트 페이지 호출시
+                            state속성값으로 객체를 보내어 값을 전달함!
+                            도착페이지인 Detail.js 컴포넌트에
+                            페이지 나타내야할 데이터 항목을
+                            데이터 속성명과 같은 이름으로 셋팅하여
+                            라우터 전달 state객체에 담아서 보낸다!
+                            cname은 캐릭터 이름
+                            cdes 캐릭터설명
+                            cdes 캐릭터설명
+                            facts는 캐릭터명세 정보임!*/}
+                <Link
+                  to="/det"
+                  state={{
+                    cname: v.cname,
+                    cdesc: v.cdesc,
+                    facts: v.facts,
+                  }}
+                >
+                  <section className="swinbx">
+                    {/* 캐릭터이미지영역 */}
+                    <div className="catimg">
+                      <img src={v.tmsrc} alt={v.cname} />
+                    </div>
+                    {/* 동영상타이틀영역 */}
+                    <div className="cattit">
+                      <h3>{v.cname}</h3>
+                    </div>
+                  </section>
+                </Link>
+              </SwiperSlide>
+            )
+        )}
+      </Swiper>
+    </>
+  );
 }
